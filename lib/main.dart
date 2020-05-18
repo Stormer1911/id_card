a33a25145e57480f1ed0ac91b11ff71b393a9971
Müller, Christoph Mirco, BOS2018 <ChristophMirco.Mueller@bertelsmann.de>
2020-05-18 20:20:22 +0200

    refactored appBar2
    2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:idcard/pages/homeAnimated.dart';
+import 'package:idcard/pages/home.dart';
 import 'package:idcard/pages/qrCode.dart';
 import 'package:idcard/pages/settings.dart';
 import 'package:idcard/sharedPrefs.dart';
--- lib/pages/appBar.dart
+++ lib/pages/appBar.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
-class AppBar{
-
+Widget idAppBar(BuildContext context, String title) {
+  return AppBar(
+    title: Center(
+      child: FittedBox(
+        fit: BoxFit.fitWidth,
+        child: Text(title, style: Theme.of(context).primaryTextTheme.headline1),
+      ),
+    ),
+    backgroundColor: Theme.of(context).appBarTheme.color,
+  );
 }

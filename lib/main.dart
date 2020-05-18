7723e050589d7158bdb23f97f5be8b53f6af79d2
Müller, Christoph Mirco, BOS2018 <ChristophMirco.Mueller@bertelsmann.de>
2020-05-18 18:13:00 +0200

    settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:idcard/pages/homeAnimated.dart';
 import 'package:idcard/pages/qrCode.dart';
+import 'package:idcard/pages/settings.dart';
 import 'package:idcard/sharedPrefs.dart';
 
 void main() => runApp(
       MaterialApp(initialRoute: '/', routes: {
         '/': (context) => HomeAnimated(),
         'qr': (context) => QrCode(),
+        'se': (context) => Settings(),
       }),
     );

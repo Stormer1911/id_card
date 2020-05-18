15a2c84009f175219b396bb733ef31648d426c9e
Müller, Christoph Mirco, BOS2018 <ChristophMirco.Mueller@bertelsmann.de>
2020-05-18 18:29:37 +0200

    started theming

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,28 @@ import 'package:idcard/pages/qrCode.dart';
 import 'package:idcard/pages/settings.dart';
 import 'package:idcard/sharedPrefs.dart';
 
+final double letterSpacingHeader = 4;
+final double letterSpacing = 2;
+final Color text = Colors.amber;
+final Color background = Colors.grey[850];
+final Color backgroundAppBar = Colors.grey[900];
+final Color qrCodeColor = Colors.amber[400];
+
 void main() => runApp(
-      MaterialApp(initialRoute: '/', routes: {
-        '/': (context) => HomeAnimated(),
-        'qr': (context) => QrCode(),
-        'se': (context) => Settings(),
-      }),
+      MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomeAnimated(),
+          'qr': (context) => QrCode(),
+          'se': (context) => Settings(),
+        },
+        theme: ThemeData(
+          primaryColor: Colors.grey[850],
+          primaryTextTheme: TextTheme(
+            headline1: TextStyle(
+
+            ),
+          ),
+        ),
+      ),
     );
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -8,6 +8,8 @@ class Settings extends StatefulWidget {
 class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+
+    );
   }
 }
